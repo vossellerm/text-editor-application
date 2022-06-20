fc78f8c4998bebfe77e56f489efe6ce0862d7ef1
Mikayla Vosseller <vossellerm@gmail.com>
2022-06-19 20:38:16 -0700

    CSS loaders and babel

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -44,7 +44,26 @@ module.exports = () => {
     ],
 
     module: {
-      rules: [],
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
+      ],
     },
   };
 };
